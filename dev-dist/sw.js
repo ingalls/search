967c144489fa29f0bd348e0179e2c5725c61a9bb
ingalls <nick@ingalls.ca>
2025-01-15 05:29:58 -0700

    Update Deps

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.316j4idu75g"
+    "revision": "0.mr1kpetsln"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
