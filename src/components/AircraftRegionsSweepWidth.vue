20a7b8961e4f8bd508a632005e91dd3e25a951fb
ingalls <nick@ingalls.ca>
2025-01-15 05:53:51 -0700

    Add Sweep Width Modal

---
+++ src/components/AircraftRegionsSweepWidth.vue
@@ -0,0 +1,23 @@
+<template>
+    <TablerModal size='xl'>
+        <div class='modal-status bg-blue' />
+        <button
+            type='button'
+            class='btn-close'
+            aria-label='Close'
+            @click='$emit("close")'
+        />
+
+        <div class='modal-header'>
+            <div class='modal-title'>Sweep Width Calculator</div>
+        </div>
+
+    </TablerModal>
+
+</template>
+
+<script setup lang='ts'>
+import {
+    TablerModal
+} from '@tak-ps/vue-tabler'
+</script>
